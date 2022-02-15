a11e5878456bd89d579b1e2938946fb5c7badf7c
Filipe Tobias <filipe.tobias@senior.com.br>
2022-02-14 22:40:21 -0300

    Adicionar tab de pedidos

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/tabs/orders_tab.dart';
 import 'package:loja_virtual/tabs/products_tab.dart';
 import 'package:loja_virtual/widgets/cart_button.dart';
 import 'package:loja_virtual/widgets/custom_drawer.dart';
@@ -29,6 +30,20 @@ class HomeScreen extends StatelessWidget {
           floatingActionButton: CartButton(),
           drawer: CustomDrawer(_pageController),
         ),
+        Container(
+          color: Colors.yellow,
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'Meus Pedidos',
+            ),
+            centerTitle: true,
+            backgroundColor: Theme.of(context).primaryColor,
+          ),
+          body: OrdersTab(),
+          drawer: CustomDrawer(_pageController),
+        )
       ],
     );
   }
